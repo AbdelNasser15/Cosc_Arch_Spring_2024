89769939843c3f8a9b53fd7148e581b520d1cf36
Abdel Nasser <AbdelDavidNasser@gmail.com>
2024-01-30 21:14:32 +0000

    Initial part1_exercises

--- Unit2_Assembly_Basics/part1_exercises.asm
+++ Unit2_Assembly_Basics/part1_exercises.asm
@@ -23,7 +23,51 @@ asm_main:
         enter   0,0               ; setup routine
         pusha
 
-		; TODO: add code
+	;	dump_regs 1
+         ;      call print_nl
+     ;   dump_regs 2
+
+;mov eax, 0
+;mov ah, 18
+;mov al, 0BAH 
+;dump_regs 1
+;call print_nl
+
+;mov ax, 4
+;dump_regs 2
+
+;mov eax,3 ;eax=3
+;mov ebx,4 ;eax =4 
+;add eax, ebx ;eax+= ebx, eax =7
+;dump_regs 1
+
+;mov eax, 34
+;sub eax, 19 ;immediate opperand
+;dump_regs 1
+
+;inc 23 ; Error
+
+;mov eax 16
+;call print_nl
+;dump_regs 1
+
+;sub eax 4
+;call print_nl
+;dump_regs 2
+
+;mov ebx, eax
+;inc ebx
+;dump_regs 3
+
+mov eax 4
+mov ebx, 4
+
+add ebx, eax ;2*4 = 8 (2a)
+add ebx, ebx ;2*8 =16 = 4a
+
+call print_nl
+
+add eax 4*2
 
         popa
         mov     eax, 0            ; return back to C
